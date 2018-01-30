af1c8f6267fb88a60e6bc8a7abc1bb52b8a43f74
Lucas Gossé <lucasgosse@Lucass-MacBook-Pro.local>
2018-01-30 18:18:06 +0100

    [FIX] Parties can no longer have the same name nor be empty

--- src/client/reducers/alert.js
+++ src/client/reducers/alert.js
@@ -1,9 +1,11 @@
-import { ALERT_POP } from "../../actionsTypes";
+import { ALERT_POP, ALERT_RESET } from "../../actionsTypes";
 
 const alert = (state = {}, action) => {
   switch (action.type) {
     case ALERT_POP:
       return { message: action.message };
+    case ALERT_RESET:
+      return {};
     default:
       return state;
   }
