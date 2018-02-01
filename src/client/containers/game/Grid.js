b5119fef79811d51cf96418128ce8cb55ee5212a
Thomas BAYET <tbayet@e3r5p19.42.fr>
2018-02-01 11:54:48 +0100

    calque

--- src/client/containers/game/Grid.js
+++ src/client/containers/game/Grid.js
@@ -4,6 +4,10 @@ import styled, { keyframes } from 'styled-components';
 import Square from "../../components/game/Square";
 
 export const Grid = ({party, player}) => {
+    player.piece = {
+        x: 5,
+        y: 5
+    }
     player.grid = [
         [0,0,0,0,0, 0,0,0,0,0],
         [0,0,0,0,0, 0,0,0,0,0],
@@ -77,8 +81,32 @@ export const Grid = ({party, player}) => {
         backgroundColor: 'rgba(0, 0, 0, 0.6)'
     };
 
+    const calqueStyle = {
+        zIndex: '9000',
+        position: 'absolute',
+        height: '80vh',
+        minHeight: '80vh',
+        width: '40vh',
+        minWidth: '40vh'
+    }
+    const Calque = () => {
+        const pieceStyle = {
+            position: 'absolute',
+            marginTop: player.piece.y * 4  + 'vh',
+            marginLeft: player.piece.x * 4 + 'vh',
+            height: '4vh',
+            minHeight: '4vh',
+            width: '4vh',
+            minWidth: '4vh',
+            backgroundColor: 'pink'
+        }
+        const piece = <div style={pieceStyle}></div>;
+        return <div style={calqueStyle}>{piece}</div>;
+    }
+    
     return (
         <div style={styleGrid}>
+            <Calque />
             {grid}
         </div>
     );
