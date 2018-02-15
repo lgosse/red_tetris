3b5d9afa1f938038a233d500109aac00a22fbce6
Lucas Gossé <lucasgosse@Lucass-MacBook-Pro.local>
2018-02-15 13:13:52 +0100

    [UPD]

--- params.prod.js
+++ params.prod.js
@@ -3,7 +3,7 @@ const params = {
     host: 'ec2-52-47-162-238.eu-west-3.compute.amazonaws.com',
     port: 80,
     get url() {
-      return 'https://' + this.host + ':' + this.port;
+      return 'http://' + this.host + ':' + this.port;
     }
   }
 };
