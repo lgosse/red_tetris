8e822581d76a2d9a03e92616b2fc1e06523f4421
Lucas Gossé <lucasgosse@MacBook-Pro-2.local>
2018-02-27 18:27:49 +0100

    [TEST] Added all tests for forms

--- test/react/containers/forms.js
+++ test/react/containers/forms.js
@@ -1,9 +1,6 @@
 import chai from 'chai';
-import { expect } from 'chai';
-import equalJSX from 'chai-equal-jsx';
 import React from 'react';
 import { shallow } from 'enzyme';
-import sinon, { assert } from 'sinon';
 
 // PlayerForm
 import {
@@ -15,64 +12,262 @@ import {
 import { PLAYER_UPDATE, PLAYER_SAVE } from '../../../src/actionsTypes';
 
 import { updatePlayer, savePlayer } from '../../../src/client/actions/player';
+import {
+  PartyForm,
+  mapStateToPartyFormProps,
+  mapDispatchToPartyFormProps
+} from '../../../src/client/containers/forms/PartyForm';
+import {
+  Messages,
+  Chat,
+  mapStateToChatProps,
+  mapDispatchToChatProps
+} from '../../../src/client/containers/forms/Chat';
 
-describe('PlayerForm', () => {
-  const PLAYER = {
-    nickname: 'test'
-  };
-  describe('PlayerForm component', () => {
-    it('should render as expected', () => {
-      const output = shallow(<PlayerForm player={{ nickname: undefined }} />);
-      output.should.matchSnapshot();
-    });
-    it('should trigger changeNickname on typing into the input', done => {
-      const event = { target: { value: 'yolo' } };
-      const changeNickname = (event, player) => {
-        event.should.equal(event);
-        done();
-      };
-      const output = shallow(
-        <PlayerForm player={PLAYER} changeNickname={changeNickname} />
-      );
-      output.find('#nicknameInput').simulate('change', event);
-    });
-  });
-  describe('mapStateToPlayerProps', () => {
-    it('should map player to PlayerForm props', () => {
-      const state = { player: PLAYER };
-      const playerFormProps = mapStateToPlayerFormProps(state);
+chai.should();
 
-      playerFormProps.should.be.deep.equal({
-        player: PLAYER
+describe('Forms', () => {
+  describe('PlayerForm', () => {
+    const PLAYER = {
+      nickname: 'test'
+    };
+    describe('PlayerForm component', () => {
+      it('should render as expected', () => {
+        const output = shallow(<PlayerForm player={{ nickname: undefined }} />);
+        output.should.matchSnapshot();
+      });
+      it('should trigger changeNickname on typing into the input', done => {
+        const event = { target: { value: 'yolo' } };
+        const changeNickname = (event, player) => {
+          event.should.equal(event);
+          done();
+        };
+        const output = shallow(
+          <PlayerForm player={PLAYER} changeNickname={changeNickname} />
+        );
+        output.find('#nicknameInput').simulate('change', event);
+      });
+    });
+    describe('mapStateToPlayerProps', () => {
+      it('should map player to PlayerForm props', () => {
+        const state = { player: PLAYER };
+        const playerFormProps = mapStateToPlayerFormProps(state);
+
+        playerFormProps.should.be.deep.equal({
+          player: PLAYER
+        });
+      });
+    });
+    describe('mapDispatchToPlayerFormProps', () => {
+      it('should map changeNickname function to PlayerForm props', done => {
+        global.document = {
+          getElementById() {
+            return {
+              value: {
+                trim() {
+                  return 'toto';
+                }
+              }
+            };
+          }
+        };
+        const dispatch = action => {
+          action.player.should.deep.equal({ nickname: 'toto' });
+          done();
+        };
+        const { changeNickname } = mapDispatchToPlayerFormProps(dispatch);
+        changeNickname(dispatch);
       });
     });
   });
-  describe('mapDispatchToPlayerFormProps', () => {
-    it('should map changeNickname function to PlayerForm props', done => {
-      global.document = {
-        getElementById() {
-          return {
-            value: {
-              trim() {
-                return 'toto';
-              }
+
+  describe('PartyForm', () => {
+    describe('PartyForm', () => {
+      it('should render as expected', () => {
+        const player = {
+          nickname: 'toto'
+        };
+        const output = shallow(<PartyForm player={player} />);
+        output.should.matchSnapshot();
+      });
+    });
+    describe('mapStateToPartyFormProps', () => {
+      const state = {
+        player: {
+          nickname: 'toto'
+        },
+        toto: 'lala'
+      };
+
+      const props = mapStateToPartyFormProps(state);
+      props.should.deep.equal({
+        player: {
+          nickname: 'toto'
+        }
+      });
+    });
+    describe('mapDispatchToPartyFormProps', () => {
+      describe('createParty', () => {
+        it('should map dispatch as expected', done => {
+          global.document = {
+            getElementById() {
+              return {
+                value: {
+                  trim() {
+                    return 'toto';
+                  }
+                },
+                checked: true
+              };
             }
           };
-        }
-      };
-      const dispatch = action => {
-        action.player.should.deep.equal({ nickname: 'toto' });
-        done();
-      };
-      const { changeNickname } = mapDispatchToPlayerFormProps(dispatch);
-      changeNickname(dispatch);
+          const dispatch = action => {
+            console.log(action);
+            action.party.should.deep.equal({
+              name: 'toto',
+              size: 'toto',
+              withBonus: true,
+              players: []
+            });
+            done();
+          };
+
+          const { createParty } = mapDispatchToPartyFormProps(dispatch);
+          createParty({ preventDefault() {} });
+        });
+      });
     });
   });
-});
 
-describe('PartyForm', () => {
-  describe('PartyForm', () => {
-    it('should render as expected', () => {});
+  describe('Chat', () => {
+    describe('Messages', () => {
+      it('should render as expected', () => {
+        global.document = {
+          getElementById() {
+            return {
+              scrollTop: 2,
+              scrollHeight: 300
+            };
+          }
+        };
+        const player = {
+          socketId: 'unikID1',
+          nickname: 'awesomeNickname'
+        };
+        const messages = [
+          {
+            senderId: 'unikID1',
+            senderName: 'awesomeNickname',
+            text: 'yolooooo'
+          },
+          {
+            senderId: 'unikID2',
+            senderName: 'awesomeNicknameeee',
+            text: 'yalaaaaa'
+          }
+        ];
+
+        const output = shallow(
+          <Messages player={player} messages={messages} />
+        );
+        output.should.matchSnapshot();
+      });
+    });
+    describe('Chat', () => {
+      it('should render as expected with messages', () => {
+        const player = {
+          socketId: 'unikID1',
+          nickname: 'awesomeNickname'
+        };
+        const messages = [
+          {
+            senderId: 'unikID1',
+            senderName: 'awesomeNickname',
+            text: 'yolooooo'
+          },
+          {
+            senderId: 'unikID2',
+            senderName: 'awesomeNicknameeee',
+            text: 'yalaaaaa'
+          }
+        ];
+
+        const output = shallow(<Chat player={player} messages={messages} />);
+        output.should.matchSnapshot();
+      });
+      it('should render as expected without messages', () => {
+        const player = {
+          socketId: 'unikID1',
+          nickname: 'awesomeNickname'
+        };
+        const messages = null;
+
+        const output = shallow(<Chat player={player} messages={messages} />);
+        output.should.matchSnapshot();
+      });
+    });
+    describe('mapStateToChatProps', () => {
+      it('should map state as exptected', () => {
+        const state = {
+          party: {
+            messages: ['toto', 'tata']
+          },
+          player: {
+            nickname: 'tata'
+          },
+          ljhabvdhfi: 'askdhjfvjha'
+        };
+
+        const props = mapStateToChatProps(state);
+        props.should.deep.equal({
+          messages: ['toto', 'tata'],
+          player: {
+            nickname: 'tata'
+          }
+        });
+      });
+    });
+    describe('mapDispatchToChatProps', () => {
+      describe('handleSubmit', () => {
+        it('should handle dispatch as expected', done => {
+          global.document = {
+            getElementById() {
+              return {
+                value: {
+                  trim() {
+                    return 'toto';
+                  }
+                }
+              };
+            }
+          };
+          const dispatch = action => {
+            done();
+          };
+
+          const { handleSubmit } = mapDispatchToChatProps(dispatch);
+          handleSubmit({ preventDefault() {} });
+        });
+        it('should not handle dispatch as expected if message empty', () => {
+          global.document = {
+            getElementById() {
+              return {
+                value: {
+                  trim() {
+                    return '';
+                  }
+                }
+              };
+            }
+          };
+          const dispatch = action => {
+            throw new Error('Dispatch shouldnt have been called.');
+          };
+
+          const { handleSubmit } = mapDispatchToChatProps(dispatch);
+          handleSubmit({ preventDefault() {} });
+        });
+      });
+    });
   });
-  describe('mapState', () => {});
 });
